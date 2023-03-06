363cdfcd0c9d99ea1cf07b396dbd47df81acff60
Ameag <nekqga@gmail.com>
2023-03-06 22:35:56 +0500

    Реализует структуру api

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "alvile-api/server"
+
+func main() {
+	var serverInstance server.Server
+}
---
+++ server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		MaxHeaderBytes: 1 << 20, //1 mb
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	logrus.Info("server started successfully")
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) {
+	logrus.Info("server shutdown process started")
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		logrus.Error(err.Error())
+	} else {
+		logrus.Info("http listener shutdown successfully")
+	}
+
+	logrus.Info("server shutdown process completed successfully")
+}
