7c336915fb2c7eb5a0e4720b4b1a986b88e41d83
Kuchin Aleksey <nekqga@gmail.com>
2023-03-14 22:47:07 +0500

    Реализует подключение к бд

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,43 @@ import (
 	"alvile-api/pkg/repository"
 	"alvile-api/pkg/service"
 	"alvile-api/server"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{})
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("postgres.host"),
+		Port:     viper.GetString("postgres.port"),
+		Username: viper.GetString("postgres.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("postgres.dbname"),
+		SSLMode:  viper.GetString("postgres.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewService(services)
 	var serverInstance server.Server
-	if err := serverInstance.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := serverInstance.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		logrus.Fatalf("error ocured while running http server: %s", err.Error())
 	}
+}
 
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
