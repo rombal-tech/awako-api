49cc0495717ca8afaeb6ae5c56c141902e9181a7
Kuchin Aleksey <nekqga@gmail.com>
2023-03-21 20:23:09 +0500

    Реализует генерацию пароля

--- models/account.go
+++ models/account.go
@@ -3,5 +3,5 @@ package models
 type Account struct {
 	Deleted  bool   `json:"deleted"`
 	Email    string `json:"email" binding:"required"`
-	Password string `json:"password"binding:"required" `
+	Password string `json:"password" `
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,6 +5,10 @@ import (
 	"alvile-api/pkg/repository"
 	"crypto/sha1"
 	"fmt"
+	"github.com/sethvargo/go-password/password"
+	"log"
+	"math/rand"
+	"time"
 )
 
 const salt = "5hh24h23fgfgg4g3g"
@@ -18,8 +22,24 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user models.Account) (string, error) {
-	user.Password = generatePasswordHash(user.Password)
-	return s.repo.CreateUser(user)
+	if user.Password == "" {
+		user.Password = generatePassword(user.Password)
+		//user.Password = generatePasswordHash(user.Password)
+		return s.repo.CreateUser(user)
+	} else {
+		user.Password = generatePasswordHash(user.Password)
+		return s.repo.CreateUser(user)
+	}
+}
+
+func generatePassword(inputPassword string) string {
+	var err error
+	rand.Seed(time.Now().UnixNano())
+	inputPassword, err = password.Generate(10, 5, 0, false, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return inputPassword
 }
 
 func generatePasswordHash(password string) string {
