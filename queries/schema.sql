bd0785b3ae66690e8d9e9d5bfd4a6df85cc12448
execaus <execaus@gmail.com>
2023-05-01 16:23:52 +0500

    Добавляет форматирование в описание и обновляет README

--- queries/schema.sql
+++ queries/schema.sql
@@ -1,46 +1,41 @@
 CREATE TABLE "public.Account" (
-                                  "email" character varying NOT NULL,
-                                  "password" character varying NOT NULL,
-                                  "deleted" BOOLEAN NOT NULL,
-                                  CONSTRAINT "Account_pk" PRIMARY KEY ("email")
+  "email" character varying NOT NULL,
+  "password" character varying NOT NULL,
+  "deleted" BOOLEAN NOT NULL,
+  CONSTRAINT "Account_pk" PRIMARY KEY ("email")
 ) WITH (
-      OIDS=FALSE
-      );
-
-
+  OIDS=FALSE
+  );
 
 CREATE TABLE "public.ConfirmedAccount" (
-                                           "email" character varying NOT NULL,
-                                           "code" character varying NOT NULL,
-                                           "confirmed" BOOLEAN NOT NULL,
-                                           CONSTRAINT "ConfirmedAccount_pk" PRIMARY KEY ("email")
+       "email" character varying NOT NULL,
+       "code" character varying NOT NULL,
+       "confirmed" BOOLEAN NOT NULL,
+       CONSTRAINT "ConfirmedAccount_pk" PRIMARY KEY ("email")
 ) WITH (
-      OIDS=FALSE
-      );
+  OIDS=FALSE
+  );
 
 CREATE TABLE "public.Session" (
-                                  "id" bigserial NOT NULL,
-                                  "email" character varying NOT NULL,
-                                  "session_string" character varying NOT NULL,
+      "id" bigserial NOT NULL,
+      "email" character varying NOT NULL,
+      "session_string" character varying NOT NULL,
 
-                                  CONSTRAINT "Session_pk" PRIMARY KEY ("id")
+      CONSTRAINT "Session_pk" PRIMARY KEY ("id")
 ) WITH (
-      OIDS=FALSE
-      );
+  OIDS=FALSE
+  );
 
 CREATE TABLE "public.Scheme"(
-                                "id" bigserial NOT NULL,
-                                "name" character varying NOT NULL,
-                                "description" character varying NOT NULL,
-                                "author" character varying NOT NULL,
-                                "creation_date" timestamp NOT NULL,
-                                CONSTRAINT "Scheme_pk" PRIMARY KEY ("id")
+    "id" bigserial NOT NULL,
+    "name" character varying NOT NULL,
+    "description" character varying NOT NULL,
+    "author" character varying NOT NULL,
+    "creation_date" timestamp NOT NULL,
+    CONSTRAINT "Scheme_pk" PRIMARY KEY ("id")
 ) WITH (
-      OIDS=FALSE
-      );
-
-
-
+  OIDS=FALSE
+  );
 
 ALTER TABLE "public.ConfirmedAccount" ADD CONSTRAINT "ConfirmedAccount_fk0" FOREIGN KEY ("email") REFERENCES "public.Account"("email");
 ALTER TABLE "public.Session" ADD CONSTRAINT "Session_fk0" FOREIGN KEY ("email") REFERENCES "public.Account"("email");
